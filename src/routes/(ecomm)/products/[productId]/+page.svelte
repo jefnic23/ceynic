4f1b9be0af54e28a102b5640ad8d0cbf9f0cac4f
Jeffrey Nicholas <jefnic23@gmail.com>
2025-01-26 18:01:49 -0500

    ✨ (location.ts): add Location interface to define structure for shipping location data ✨ (page.svelte): enhance product page to display shipping location and returns information 🔧 (page.ts): integrate fetchShippingLocation function to retrieve shipping location data for product page

--- src/routes/(ecomm)/products/[productId]/+page.svelte
+++ src/routes/(ecomm)/products/[productId]/+page.svelte
@@ -1,75 +1,132 @@
 <script lang="ts">
-	import type { PageData } from "./$types";
-    import PayPal from "$lib/components/PayPal.svelte";
+	import type { PageData } from './$types';
+	import PayPal from '$lib/components/PayPal.svelte';
+	import Tooltip from '$lib/components/Tooltip.svelte';
 
-    export let data: PageData;
+	export let data: PageData;
+
+    const returnsTooltipContent = `
+        You have 14 days from item delivery to ship this item back. Buyer is responsible for
+        return shipping costs. If the item is not returned in its original condition, the buyer
+        is responsible for any loss in value.
+    `;
 </script>
 
 <div class="wrapper">
-    {#await data.product}
-        <p>Loading product...</p>
-    {:then product} 
-        <img src="{product.images[0]}" alt={product.title} />
-        <div class="row">
-            <div class="column" style="flex: 3;">
-                <h1>{product.title}</h1>
-                <p>{product.description}</p>
-            </div>
-            <div class="column" style="flex: 2;">
-                <div>
-                    <div>
-                        ${product.price}
-                    </div>
-                    <div>
-                        + FREE shipping
-                    </div>
-                    <PayPal productId={product.id} />
-                </div>
-                <div>
-                    <div>
-                        ships from
-                    </div>
-                    <div>
-                        Pinckney, MI
-                    </div>
-                    <div>
-                        returns & exchanges
-                    </div>
-                    <div>
-                        You have 14 days from item delivery to ship this item back. Buyer is responsible for return shipping costs. If the item is not returned in its original condition, the buyer is responsible for any loss in value.
-                    </div>
-                </div>
-            </div>
-        </div>
-    {/await}
+	{#await data.product}
+		<p>Loading product...</p>
+	{:then product}
+		<div class="row">
+			<div class="column flex-5 center">
+				<img src={product.images[0]} alt={product.title} />
+			</div>
+			<div class="column flex-3 left">
+				<div>
+					<div class="title">{product.title}</div>
+					<div class="description">{product.description}</div>
+				</div>
+				<div>
+					<div class="price">${product.price}</div>
+					<div class="shipping">+ <i>free shipping</i></div>
+				</div>
+				<PayPal productId={product.id} />
+				<div>
+					{#await data.location}
+						<div>Ships from: <div class="loader"></div></div>
+					{:then location}
+						<div>
+							Ships from: <b>{location.city ? `${location.city}, ${location.state}` : `${location.state}`}</b>
+						</div>
+					{/await}
+				</div>
+                <Tooltip content={returnsTooltipContent.trim()} position={'bottom'}>
+                    <div class="returns">Returns & exchanges accepted within 14 days</div>
+                </Tooltip>
+			</div>
+		</div>
+	{/await}
 </div>
 
 <style>
-    .wrapper {
-        display: flex;
-        flex-direction: column;
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		width: 100%;
+		padding: 1rem 0;
+	}
+
+	.row {
+		display: flex;
+		flex-direction: row;
+		column-gap: 1rem;
+	}
+
+	.column {
+		display: flex;
+		flex-direction: column;
+		align-items: left;
+		row-gap: 1rem;
+	}
+
+    .left {
+        align-items: left;
+    }
+
+    .center {
         align-items: center;
-        width: 100%;
     }
 
-    .row {
-        display: flex;
-        flex-direction: row;
+	.flex-5 {
+		flex: 5;
+	}
+
+	.flex-3 {
+		flex: 3;
+	}
+
+	img {
+		width: 89%;
+	}
+
+	.title {
+		font-size: xx-large;
+		font-weight: bolder;
+		text-align: center;
+	}
+
+	.description {
+		text-align: justify;
+		hyphens: auto;
+	}
+
+	.price {
+		font-size: x-large;
+		font-weight: bold;
+		text-align: center;
+	}
+
+    .shipping {
+        text-align: center;
     }
 
-    .column {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
+    .loader {
+        display: inline-block;
+        width: 21px;
+        aspect-ratio: 4;
+        background: radial-gradient(circle closest-side,#000 90%,#0000) 0/calc(100%/3) 100% space;
+        clip-path: inset(0 100% 0 0);
+        animation: l1 1s steps(4) infinite;
     }
 
-    .column * {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
+    @keyframes l1 {to{clip-path: inset(0 -34% 0 0)}}
+
+    .returns {
+        text-decoration: underline dashed;
+        text-underline-position: under;
     }
 
-    img {
-        width: 89%;
+    .returns:hover {
+        cursor: help;
     }
 </style>
--- src/routes/(ecomm)/products/[productId]/+page.ts
+++ src/routes/(ecomm)/products/[productId]/+page.ts
@@ -1,4 +1,5 @@
 import { PUBLIC_API_URL } from '$env/static/public';
+import type { Location } from '$lib/interfaces/location.js';
 
 export const load = async ({ fetch, params }) => {
     const fetchProduct = async (productId: string) => {
@@ -13,7 +14,21 @@ export const load = async ({ fetch, params }) => {
         return responseData;
     }
 
+    const fetchShippingLocation = async () => {
+        // todo: fetch this on any page load and store to avoid multiple calls
+        const response = await fetch(`${PUBLIC_API_URL}/storefronts/location`);
+
+        if (!response.ok) {
+            console.log("Error retrieving shipping location.");
+        }
+
+        const responseData: Location = await response.json();
+
+        return responseData;
+    }
+
     return {
-        product: fetchProduct(params.productId)
+        product: fetchProduct(params.productId),
+        location: fetchShippingLocation()
     }
 }

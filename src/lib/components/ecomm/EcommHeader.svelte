df8dda0d88b09dc7a1912ed6cda1d0cdeb0a5be2
Jeffrey Nicholas <jefnic23@gmail.com>
2025-05-27 15:46:14 -0400

    ♻️ (EcommHeader.svelte): refactor cart link to use onclick instead of on:click for consistency and clarity in event handling

--- src/lib/components/ecomm/EcommHeader.svelte
+++ src/lib/components/ecomm/EcommHeader.svelte
@@ -78,7 +78,7 @@
 				</nav>
 			</div>
 			<div class="header-right">
-				<!-- <a href="/cart" class:active={url.pathname === '/cart'} on:click={() => {if (open) open = !open}}><Cart size={32} /></a> -->
+				<a href="/cart" class:active={url.pathname === '/cart'} onclick={() => {if (open) open = !open}}><Cart size={32} /></a>
 			</div>
 		</div>
 	{:else}
@@ -94,7 +94,7 @@
 				</nav>
 			</div>
 			<div class="header-right">
-				<!-- <a href="/cart" class:active={url.pathname === '/cart'}><Cart size={32} /></a> -->
+				<a href="/cart" class:active={url.pathname === '/cart'}><Cart size={32} /></a>
 			</div>
 		</div>
 	{/if}
